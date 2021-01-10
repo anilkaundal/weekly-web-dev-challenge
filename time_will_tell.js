4a3538c9962c239264c3d7735e5717f54e295695
Anil Kaundal <60393858+anilkaundal@users.noreply.github.com>
2021-01-10 20:33:48 +0530

    Delete time_will_tell.js

--- time_will_tell.js
+++
@@ -1,8 +0,0 @@
-/*
-Description:
-    Your job is to fix this broken clock!
-    Right now it's only right twice a day.
-
-Skills: 
-    CSS Transforms, JavaScript Dates, setTimeout()/setInterval(), HTML/CSS in JavaScript
-*/
