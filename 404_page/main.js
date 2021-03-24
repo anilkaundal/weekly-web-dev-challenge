4be1eb5bbadda5fa160e8195fc6bfcc474f471a4
Anil Kaundal <60393858+anilkaundal@users.noreply.github.com>
2021-03-24 22:34:15 +0530

    Create 404_page

---
+++ 404_page/main.js
@@ -0,0 +1,17 @@
+$("button").mousedown(function () {
+    $(this).html("Redirecting...").velocity({
+        backgroundColorRed: "mediumseagreen",
+        translateY: "-1.5rem",
+        rotateZ: "-10deg"
+    }, 100, "easeOut").velocity({
+        rotateZ: "8deg",
+    }, 150).velocity({
+        translateY: "0",
+        rotateZ: "0"
+    }, 600, "easeOutBounce");
+
+    $("+ .shadow", this).velocity({
+        scale: "1.3",
+        opacity: "1"
+    }, 150).velocity("reverse", 600, "easeOutBounce");
+});
